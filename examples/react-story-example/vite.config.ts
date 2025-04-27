2f3a66ac3408e65a376ffaa2cbf27399d87a1b12
cqh963852 <cqh963852@hotmail.com>
2025-04-27 16:49:49 +0800

    chore: update config

--- examples/react-story-example/vite.config.ts
+++ examples/react-story-example/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { reactStoryPlugin } from "@escharm/story";
+import { reactStoryPlugin } from "@escharm/story/react";
 
 // https://vite.dev/config/
 export default defineConfig({
