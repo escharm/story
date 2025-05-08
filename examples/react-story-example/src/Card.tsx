5d336657f6c15d9518529b27de4281ca23b0e3b8
cqh963852 <cqh963852@hotmail.com>
2025-05-08 16:18:04 +0800

    feat: update project

--- examples/react-story-example/src/Card.tsx
+++ examples/react-story-example/src/Card.tsx
@@ -4,12 +4,12 @@ const Card = () => {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="card">
-      <button onClick={() => setCount((count) => count + 1)}>
+    <div data-id="KwFWRRU3i27PY2VjHwJXb" className="card">
+      <button data-id="nEA6O2eNC-1Kbcf2xqDaq" onClick={() => setCount((count) => count + 1)}>
         count is {count}
       </button>
-      <p>
-        Edit <code>src/App.tsx</code> and save to test HMR
+      <p data-id="w0HwBwK5RAoIKkfh9xo96">
+        Edit <code data-id="D5bHYwz7uEQrjHJY2eAPf">src/App.tsx</code> and save to test HMR
       </p>
     </div>
   );
--- examples/react-story-example/vite.config.ts
+++ examples/react-story-example/vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { reactStoryPlugin } from "@escharm/story/react";
+import { tsImport } from "tsx/esm/api";
+import path from "path";
+
+const { reactStoryPlugin } = await tsImport("@escharm/story", import.meta.url);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), reactStoryPlugin()],
+  resolve: {
+    alias: {
+      "@escharm/story-editor": path.resolve(
+        __dirname,
+        "../../packages/story-editor/src",
+      ),
+    },
+  },
 });
